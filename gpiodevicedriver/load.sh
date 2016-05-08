14ad95a2abe1b07884dbd19b8c99265704a2fc66
hung nguyen <hungnvk54@gmail.com>
2016-05-08 04:09:06 +0000

    add the gpio device driver version 1

---
+++ gpiodevicedriver/load.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+module="raspy_io.ko"
+device="io_dev"
+
+/sbin/insmod $module 
+
+major=`cat /proc/devices | awk "{if(\\$2==\"$device\")print \\$1}"`
+rm -f /dev/${device} c $major 0
+
+mknod /dev/${device} c $major 0
+
+chmod 666 /dev/${device}
